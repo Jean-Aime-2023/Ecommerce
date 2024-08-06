1577105ae9bd8a973856ad4eab1ffcdf22ee8b98
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-08-07 01:30:43 +0200

    updates : landing page basics

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import HeroImg from '../../assets/images/image 53.png'
+import svg from '../../assets/icons/Rectangle 18.svg'
 const Hero = () => {
   return (
     <div className='flex flex-col w-full md:flex-row md:flex-1 md:gap-8 justify-between items-center gap-10 xl:max-w-[1470px] md:max-w-[1570px] md:mx-auto md:pl-12 md:mt-[5rem] max-md:px-10'>
       <div className='flex flex-col gap-8 max-md:text-center nunito-sans md:w-[40%]'>
+        <section className='flex flex-row items-center gap-2'>
         <h2 className='text-4xl font-extrabold lg:text-[45px] leading-[50px]'>Unite your <span className='text-[#DB4444]'>Shopping:</span> <br /> Diverse Products from <br /> <span className='text-[#DB4444]'>Trusted</span> Companies</h2>
+        <img src={svg} alt='img' />
+        </section>
         <div className='flex flex-col gap-4 max-w-[550px]'>
           <p className='text-xl md:text-[20px]'>Our platform ensures a safe and secure shopping experience with 100% buyer protection.</p>
           <p className='text-xl md:text-[20px]'>Lorem ipsum dolor sit amet consectetur. Metus ultrices praesent diam amet. Felis est faucibus vulputate at augue.Lorem ipsum dolor sit amet consectetur.</p>
